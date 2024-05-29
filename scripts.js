4915486deffb6e2939c22b87ee9a3561d0390770
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-05-29 23:50:08 +0100

    updstes

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var i = 0;
+var txt = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis corrupti esse cupiditate, porro consequatur aperiam libero quibusdam dolorem nulla facere? Aspernatur, sapiente nesciunt ratione alias reiciendis ipsum ullam consequatur exercitationem.'
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.lenght) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+const progressBar = document.querySelector('.progress-bar');
+const progressValue = 50; // Example: 50% progress
+
+progressBar.style.width = `${progressValue}%`;
+
+
+progressBar.style.width = `${progressValue}%`;
